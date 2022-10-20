ddd0f8ee707f1a274b8cd9e650204e83da447f00
sabakhan50 <116193886+sabakhan50@users.noreply.github.com>
2022-10-20 23:17:51 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,108 @@
+/*let a=[1,22,54,65,76,67];
+let b=a.length;
+console.log(b);
+*/
+/*
+let a=[1,22,54,65,76,67];
+let b=a.reverse();
+console.log(b);
+*/
+/*let a=[1,22,54,65,76,67];
+let b=a.sort();
+console.log(b);
+*/
+/*let a=[1,22,54,65];
+let b=a.fill("banana");
+console.log(b);
+*/
+/*let a=["this","is","an","array"];
+let b=a.join(" ");
+console.log(b);
+*/
+/*let a=["javascript","array","methods","34"];
+let b=a.toString();
+console.log(b);
+*/
+/*let a=["javascript","array"];
+let b=a.push("methods");
+console.log(a);
+*/
+/*let a=[1,22,54,657];
+let b=a.push(34);
+console.log(a);
+*/
+/*let a=[1,22,54,65,76,67];
+let b=a.pop();
+console.log(a);
+*/
+/*
+let a=[1,22,54,65,76,67];
+let b=a.shift();
+console.log(a);
+*/
+/*
+let a=[1,22,54,65,76,67];
+let b=a.splice(1,2);
+console.log(a);
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.lastIndexOf());
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.lastIndexOf(65));
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.indexOf(76));
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.includes(54));
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.includes(55));
+*/
+/*let a=[1,22,54,65,76,67];
+console.log(a.slice(2,5));
+console.log(a.slice(0,3));
+console.log(a.slice(0,));
+*/
+/*function even(a){
+    return a % 2 != 0; 
+}
+let num = [2,9,6,7];
+console.log(num.findIndex(even));
+*/
+/*const num = [1,2,3,8];
+function sum(a,b){
+    return a + b;
+}
+console.log(num.reduceRight(sum));
+*/
+/*const name = ["apple","banana","lychee"];
+console.log(Array.isArray(name));
+const l1 = ("mango");
+console.log(Array.isArray(l1));
+*/
+/*let nums = [1,2,4,8,4];
+function square(num){
+    return num * num; 
+}
+console.log(nums.map(square));
+*/
+/*let nums = [1,2,4,8,4];
+function square(num){
+    console.log(num * num); 
+}
+nums.forEach(square);
+*/
+/*function even(a){
+    return a % 2 == 0; 
+}
+let num = [2,9,6,7];
+console.log(num.some(even));
+*/
+/*let num = [1,2,4,8,4];
+function even(num){
+    return num % 2 == 0; 
+}
+console.log(num.filter(even));
+*/
