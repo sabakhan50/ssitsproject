7d5ac8d53ccee55a45d15a767777129d74b82a25
sabakhan50 <116193886+sabakhan50@users.noreply.github.com>
2022-10-20 23:36:28 +0530

    Add files via upload

---
+++ stringmeth.js
@@ -0,0 +1,54 @@
+
+/*let m = "saba--khan";
+let length=m.length;
+console.log(length);
+*/
+/*
+let m = "mayana ";
+let rep = m.replace('','saba');
+console.log(rep);
+*/
+/*const name = "mayana saba";
+const s1 = name.slice(0,6);
+console.log(s1);
+*/
+/*const name = "mayana saba";
+const s1 = name.trim();
+console.log(s1);
+*/
+/*const name = "mayana:saba:khan";
+const s1 = name.split(":");
+console.log(s1);
+*/
+/*const name = "mayana saba khan";
+const s1 = name.split(0,5);
+console.log(s1);
+*/
+/*const name = "";
+const s1 = name.concat("saba khan");
+console.log(s1);
+*/
+/*const name = " mayana ";
+const s1 = name.concat("saba khan");
+console.log(s1);
+*/
+/*const name = "mayana saba khan";
+const s1 = name.search("saba");
+console.log(s1);
+*/
+/*const name = "mayana saba khan";
+const s1 = name.search("khanam");
+console.log(s1);
+*/
+/*const name = "mayana saba khan";
+const s1 = name.substring(0,6);
+console.log(s1);
+*/
+/*const name = "mayana";
+const s1 = name.toUpperCase();
+console.log(s1);
+*/
+/*const name = "MAYANA";
+const s1 = name.toLowerCase();
+console.log(s1);
+*/
